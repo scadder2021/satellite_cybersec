802827796de1f9b765bec1f028a325891565707b
scadder2021 <80378837+scadder2021@users.noreply.github.com>
2021-04-09 16:49:32 -0600

    Update Flight_Software.c

--- MIL-STD-1553_Emulation/Flight_Software.c
+++ MIL-STD-1553_Emulation/Flight_Software.c
@@ -31,7 +31,7 @@ void generate_random_data(int rt_address)
     }
     else if (word_type == 1)
     {
-        request_data_from_rt(rt_address, RANDOM(1, 30), RANDOM(1, 62));
+        request_data_from_rt(rt_address, RANDOM(1, 15), RANDOM(1, 32));
     }
 }
 
@@ -73,14 +73,14 @@ void take_picture()
 void check_telemetry()
 {
     printf("checking telem");
-    request_data_from_rt(MULTIPLEXOR, RANDOM(1, 15), RANDOM(1, 32));
+    request_data_from_rt(MULTIPLEXOR, 1, RANDOM(1, 10));
 }
 
 void check_status_sc_systems()
 {
     printf("check status");
-    request_data_from_rt(STARTRACKER, RANDOM(1, 15), RANDOM(1, 32));
-    request_data_from_rt(RADIO, RANDOM(1, 15), RANDOM(1, 32));
+    request_data_from_rt(STARTRACKER, 1, RANDOM(1, 10));
+    request_data_from_rt(RADIO, 1, RANDOM(1, 10));
 }
 
 /* =======================================================
@@ -95,39 +95,8 @@ int main()
 {
     init_bc();
     sleep(1);
-    //clock_t time_at_start = clock();
-    //clock_t time_over_target = clock();
     while (1)
     {
-        /* Since we are not modeling satellite orbits in this code,
-           time elapsed is used as a proxy for a completed orbit.
-           LEO satellites take approx. 90 minutes to orbit the earth
-           and so are over the ground station every 90 minutes. This
-           has been reduced to 9 minutes in this simulation. */
-        /*
-        clock_t current_time = clock();
-        int send_ground_station_command = RANDOM(1, 100);
-
-        if(current_time - time_at_start == 9)
-        {
-            point_sc_at_groundstation();
-            send_data_to_ground();
-            time_at_start = clock();
-        }
-        else if(current_time - time_at_start == 5)
-        {
-            point_sc_at_target();
-            take_picture();
-        }
-        else if (send_ground_station_command <= PERCENT_STATION_COMMANDS)
-        {
-            generate_random_data((rand() % 6) + 1); 
-        }
-        else
-        {
-        */
-            //mode_code_e mode_code1 = RANDOM(0, 8);
-            //mode_code_e mode_code2 = RANDOM(10, 15);
             
             check_telemetry();
             sleep(10);
@@ -138,7 +107,7 @@ int main()
             sleep(10);
             point_sc_at_target();
             take_picture();
-            sleep(10);
+            sleep(300);
            
 
  
